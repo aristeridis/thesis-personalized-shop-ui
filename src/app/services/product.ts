50070c184015eb930209dba3b5e31df0d11b3d22
Alexandros Aristeridis <aristeridis@hotmail.gr>
2026-03-17 21:47:57 +0200

    product model and setting up http connection with backend

---
+++ src/app/services/product.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:8080/api/products';
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+}
